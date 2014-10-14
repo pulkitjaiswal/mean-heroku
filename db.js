903bdc70af811083750603e3219c59834f3e773d
Pulkit Jaiswal <pulkitj@icloud.com>
2014-10-15 05:37:48 +0800

    mongo URI

--- db.js
+++ db.js
@@ -1,5 +1,5 @@
 var mongoose = require('mongoose')
-mongoose.connect('mongodb://localhost/social', function () {
+mongoose.connect(process.env.MONGOLAB_URI ||'mongodb://heroku_app30701591:frvr53f8jvjm6j3gsbch4kjhe7@ds035750.mongolab.com:35750/heroku_app30701591', function () {
   console.log('mongodb connected')
 })
 module.exports = mongoose
